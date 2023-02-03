e08d759c118790cb30ddc3130986c2269f961164
tenochg <yomoozuki@gmail.com>
2023-02-02 16:58:43 -0800

    Lab 3

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
+    val1 = int(input('First Integer value:'))
+    val2 = int(input('Second Integer value:'))
+    val3 = int(input('Third Integer value:'))
+    sum = val1 + + val2 + val3
+    avg = sum/3
+
+    print(f'Values: {val1} {val2} {val3}')
+    print(f'Total: {sum}')
+    print(f'Average: {avg:.2f}')
 
     pass
 
